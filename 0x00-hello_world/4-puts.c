e09261ffda79a5c0cb8af00030ca3f423ac12dc8
AmsaleTsige <amsale324@gmail.com>
2022-06-30 12:43:17 -0700

    hello puts is done

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,19 +1,10 @@
-#include <stdio.h>
-
-/
-
- * main - Entry point
- *
- *  * Return: Always 0 (sucess)
- *
- *   */
-
- int main(void)
-
+#include<stdio.h>
+/*
+*main - Entry point
+*Return: Always 0 (sucess)
+*/
+int main(void)
 {
-
-	puts("\"Programming is like building a multilingual puzzle");
-
-	return (0);
-
+	puts("\"programming is like building a multilingual puzzle");
+	return 0;
 }
