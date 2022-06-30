fe6f58c163e166657fd9d432ee03e98fefdf650a
AmsaleTsige <amsale324@gmail.com>
2022-06-30 16:04:40 -0700

    size

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,12 @@
-#include<stdio.h>
+#include <stdio.h>
 #include <string.h>
 /*
- *main - Entry point
- *Description:prints"\"with proper grammer,but the outcomeisapieceofart"*
- *Return:Always 0(success)
- */
+*main-Entry point*
+*Description:prints"\"with proper grammer,but the outcomeisapieceofart"*
+*Return:Always 0(success)
+*/
 int main(void)
 {
-     printf("with proper grammar, but the outcome is a piece of art,\n");
-     return (0);
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
 }
