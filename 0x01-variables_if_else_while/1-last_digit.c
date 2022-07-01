d49a1db2110b3254763d8e88b54f71ebf9da2391
AmsaleTsige <amsale324@gmail.com>
2022-07-01 10:37:14 -0700

    I sometimes suffer from insomnia

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -2,9 +2,9 @@
 #include <stdlib.h>
 #include <time.h>
 /**
-*main - Entry point
-*Return: Always 0 (Success)
-*/
+* main - Entry point
+* Return: Always 0 (Success)
+**/
 int main(void)
 {
 int n;
@@ -13,11 +13,11 @@ char str[] = "Last digit of";
 srand(time(0));
 n = rand() - RAND_MAX / 2;
 L = n % 10;
-if (L > s)
-printf("%s %d is %d and is greater than 5\n", str, n, L);
+if (L > 5)
+	printf("%s %d is %d and is greater than 5\n", str, n, L);
 else if (L == 0)
 printf("%s %d is %d and is 0\n", str, n, L);
 else if (L < 6)
-printf("%s %d is %d and is less than 6 and 0\n", str, n, L);
+printf("%s %d is %d and is less than 6 and not 0\n", str, n, L);
 return (0);
 }
