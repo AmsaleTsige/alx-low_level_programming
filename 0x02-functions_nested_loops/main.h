f9ece6fdbb6f5f055abf5c2f555f79e5775e2e42
AmsaleTsige <amsale324@gmail.com>
2022-07-05 08:53:40 -0700

    main.h for printing alphabet

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,12 +1,1 @@
-int _putchar(char k);
-void print_alphabet(void);
 void print_alphabet_x10(void);
-int _islower(int c);
-int _isalpha(int c);
-int print_sign(int n);
-int _abs(int a);
-int print_last_digit(int a);
-void jack_bauer(void);
-void times_table(void);
-int add(int a, int b);
-void print_to_98(int n);
