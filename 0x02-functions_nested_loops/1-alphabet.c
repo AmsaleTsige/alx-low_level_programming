5643ebb15d137f5313c1b593d9bf632241d31fc2
AmsaleTsige <amsale324@gmail.com>
2022-07-05 06:44:37 -0700

    mainh

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,4 +1,5 @@
-#include "holberton.h"
+#include <stdio.h>
+#include "main.h"
 /**
 * print_alphabet - a function that prints the alphabet, in lowercase
 * Return: Always 0 (Succes)
@@ -6,7 +7,7 @@
 void print_alphabet(void)
 {
 int n;
-for (n = 'a'; n <= 'z'; n++)
+for(n = 'a'; n <= 'z'; n++)
 {
 _putchar(n);
 }
