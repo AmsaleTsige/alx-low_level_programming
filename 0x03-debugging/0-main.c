9137f57234a5840f6b942352eecbdab8aacf0943
AmsaleTsige <amsale324@gmail.com>
2022-07-06 14:50:41 -0700

    pultiple

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -2,6 +2,7 @@
 
 /**
  * main - will check for postive_or_negative
+ * main Description - test function
  * Return: 0 up on success
  */
 int main(void)
--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -4,5 +4,4 @@
 #include <stdio.h>
 
 void positive_or_negative(int i);
-
 #endif /* MAIN_H */
