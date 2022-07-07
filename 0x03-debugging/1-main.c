cd06ea156ac5811d823e71b27bb50e00639681a5
AmsaleTsige <amsale324@gmail.com>
2022-07-07 00:19:19 -0700

    largest number

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 /**
 * main - causes an infinite loop
+* Description 
 * Return: 0
 */
 int main(void)
@@ -9,13 +10,11 @@ int i;
 printf("Infinite loop incoming :(\n");
 i = 0;
 /**
-* while (i < 10)
-*
-* {
-* putchar(i);
-*
-}
-*/
+ * while (i < 10)
+ * {
+ * putchar(i);
+ *}
+ */
 printf("Infinite loop avoided! \\o/\n");
 return (0);
 }
