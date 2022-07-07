8e116581b59fcfa742e47d06ab10150644dbeb35
AmsaleTsige <amsale324@gmail.com>
2022-07-07 07:28:51 -0700

    largest of three integres

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -1,23 +1,23 @@
 #include "main.h"
 /**
- * largest_number - returns the largest of 3 numbers
+ * largest_number -returns the largest of 3 numbers
  * @a: first integer
  * @b: second integer
  * @c: third integer
  * Return: largest number
-*/ 
+ */
 int largest_number(int a, int b, int c)
 {
 int largest;
-if (a > b && b > c)
-largest = a;
+if (a>b && b>c)
+largest =a;
 else if (a > b && b < c)
 {
 if (a > c)
 largest = a;
 else
 largest = c;
-}	
+}
 else if (b > a && a > c)
 largest = b;
 else if (b > a && a < c)
