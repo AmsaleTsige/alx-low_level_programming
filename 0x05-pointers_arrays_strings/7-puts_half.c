16c7a4b5af48a28e40fd9cdf618e4e8d211ec728
AmsaleTsige <amsale324@gmail.com>
2022-07-11 11:34:05 -0700

    Arrays are not poiners

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,18 +1,20 @@
 #include "main.h"
-#include <string.h>
 /**
- * puts_half - print second half of a string
- * @str: char array string typr
- * Description: If odd number of charts, prints (length -1) / 2
- */
+* puts_half - print second half of a string
+* @str: char array string typr
+* Description: If odd number of charts, prints (length -1) / 2
+*/
 void puts_half(char *str);
 {
-	int i
-	for (i = 0; str[i] != '\0'; i++);
-	i++;
-	for (i /= 2; str[i] != '\0'; i++)
-	{
-		_putchar(str[i]);
-	}
-	_putchar('\n');
+int i;
+{
+for (i = 0; str[i] != '\0'; i++)
+;
+i++;
+}
+for (i /= 2; str[i] != '\0'; i++)
+{
+_putchar(str[i]);
+}
+_putchar('\n');
 }
