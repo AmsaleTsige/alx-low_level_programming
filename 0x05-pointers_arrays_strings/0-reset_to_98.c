58d600a88dfb622f6bdbbcbf4996aa064763c1e6
AmsaleTsige <amsale324@gmail.com>
2022-07-11 05:46:53 -0700

    reset to 98

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,1 +1,10 @@
-98 Battery st. Write a function that takes a pointer to an int as parameter and updates the value it points to to 98.
+#include "main.h"
+/**
+* reset_to_98 -> a function to update the reference of a pointer
+* @n: pointer to n to be updated
+* Return: n
+*/
+void reset_to_98(int *n)
+{
+*n = 98;
+}
