ca8e96c46cfdcf443ff9430577fbe4073e7835f1
AmsaleTsige <amsale324@gmail.com>
2022-07-11 10:43:50 -0700

    reverses

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,14 +1,14 @@
 #include "main.h"
 #include <string.h>
 /**
- * puts2 -> puts2 function
- *@str :str parameter
+* puts2 -> puts2 function
+* @str :str parameter
 */
-void puts2(char *str);
+void puts2(char *str)
 {
 int len, i;
 len = strlen(str);
 for (i = 0; i < len; i += 2)
-	_putchar(str[i]);
-_putchar('\'n') ;
+_putchar(str[i]);
+_putchar('\n');
 }
