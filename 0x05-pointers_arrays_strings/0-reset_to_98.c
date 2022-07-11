1abbe6edcf6f9b913a326dc75d600ae7d1ae297f
AmsaleTsige <amsale324@gmail.com>
2022-07-11 06:26:40 -0700

    swaping is easy

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,9 +1,10 @@
 #include "main.h"
 /**
-* reset_to_98 -> a function that takes a pointer to an int as parameter and updates the value it points to to 98
-* @n: pointer to n to be updated
+* reset_to_98 - a function that takes a pointer to an int
+* @n: input
+* Return: n
 */
 void reset_to_98(int *n)
 {
-*n = 98;
+	*n = 98;
 }
