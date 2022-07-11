2bcc6baabe19f2ac63bed9f2dd4049409f4df58e
AmsaleTsige <amsale324@gmail.com>
2022-07-11 07:34:16 -0700

    puts

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <string.h>
+/**
+* _puts - Write a function that prints a string, followed
+* by a new line, to stdout.
+* @str: an input string
+* Return: Nothing
+*/
+void _puts(char *str)
+{
+puts(str);
+}
