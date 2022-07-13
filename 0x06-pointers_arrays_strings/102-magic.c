592ca627aea9f5b501fadcf197694fc7270e0a64
AmsaleTsige <amsale324@gmail.com>
2022-07-13 13:48:03 -0700

    addition of strangeness

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,4 +1,12 @@
 #include <stdio.h>
+/**
+ * write your line of code here...                                                   
+ * Remember:                                                                         
+ * - you are not allowed to use a                                                    
+ * - you are not allowed to modify p
+ * - only one statement                                                              
+ * - you are not allowed to code anything else than this line of code                
+*/ 
 int main(void)
 {
 int n;
@@ -6,14 +14,6 @@ int a[5];
 int *p;
 a[2] = 1024;
 p = &n;
-/*
-* write your line of code here...
-* Remember:
-* - you are not allowed to use a
-* - you are not allowed to modify p
-* - only one statement
-* - you are not allowed to code anything else than this line of code
-*/
 p[5] = 98;
 /* ...so that this prints 98\n */
 printf("a[2] = %d\n", a[2]);
