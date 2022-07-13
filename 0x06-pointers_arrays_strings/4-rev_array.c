5c3c8b687ab7d546aaff8342ba90fb8fb7fabb08
AmsaleTsige <amsale324@gmail.com>
2022-07-13 12:56:58 -0700

    4

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -1,20 +1,23 @@
 #include "main.h"
 /**
-* _strcmp - compares two strings
-* @s1: first string.
-* @s2: second string.
-* Return: 0 if s1 and s2 are equals,
-* another number if not.
-*/
-int _strcmp(char *s1, char *s2)
+ * reverse_array - reverses the content of an array
+ * of integers.
+ * @a: array.
+ * @n: number of elements of the array.
+ * Return: no return.
+ */
+
+void reverse_array(int *a, int n)
 {
-int i = 0, op = 0;
-while (op == 0)
-{
-if ((*(s1 + i) == '\0') && (*(s2 + i) == '\0'))
-break;
-op = *(s1 + i) - *(s2 + i);
-i++;4
-}
-return (op);
+	int i, j, temp;
+
+	for (i = 0; i < n - 1; i++)
+	{
+		for (j = i + 1; j > 0; j--)
+		{
+			temp = *(a + j);
+			*(a + j) = *(a + (j - 1));
+			*(a + (j - 1)) = temp;
+		}
+	}
 }
