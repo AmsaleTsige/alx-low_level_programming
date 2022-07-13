3b9d1a6e759b23f956644230a41e5735d283a387
AmsaleTsige <amsale324@gmail.com>
2022-07-13 12:44:07 -0700

    one

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -8,18 +8,20 @@
  */
 char *_strncat(char *dest, char *src, int n)
 {
-int count = 0, count2 = 0;
-while (*(dest + count) != '\0')
-{
-count++;
-}
-while (count2 < n)
-{
-*(dest + count) = *(src + count2)
-if (*(src + count2) == '\0')
-break;
-count++;
-count2++;
-}
-return (dest);
+	int count = 0, count2 = 0;
+
+	while (*(dest + count) != '\0')
+	{
+		count++;
+	}
+
+	while (count2 < n)
+	{
+		*(dest + count) = *(src + count2);
+		if (*(src + count2) == '\0')
+			break;
+		count++;
+		count2++;
+	}
+	return (dest);
 }
