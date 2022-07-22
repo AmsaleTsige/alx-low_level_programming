144106a764ea344783f84c76a2b363615c0d7088
AmsaleTsige <amsale324@gmail.com>
2022-07-22 12:05:16 -0700

    static libraries

--- 0x09-static_libraries/driver.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-#include "main.h"
-int main(void)
-{
-int addtion = add (4, 8);
-printf("sum = %d\n", addtion);
-return 0;
-}
--- 0x09-static_libraries/lib_newfile.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>                                                  
-
-int add(int a, int b)                                               
-
-{                                                                  
-
-return a+b;                                                         
-
-}
