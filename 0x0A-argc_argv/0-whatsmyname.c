7c2120b50f7867c0b90a2477d2a7bcfec229ade0
AmsaleTsige <amsale324@gmail.com>
2022-07-22 07:13:55 -0700

    readme

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 /**
-* main - prints its name, followed by a new line.
-* @argc: number of command line arguments.
-* @argv: array that contains the program command line arguments.
-* Return: 0 - success.
+* main -  writes program name
+* @argc: arguement count
+* @argv: array of pointers to CLI arguement
+* Return: 0
 */
-int main(int argc __attribute__((unused)), char *argv[])
+int main(int argc, char *argv[])
 {
-printf("%s\n", argv[0]);
+printf("%s\n", argv[argc - 1]);
 return (0);
 }
