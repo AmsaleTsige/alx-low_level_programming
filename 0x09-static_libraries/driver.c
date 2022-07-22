7edbdf98295201406ce14508742a74b09769d6b2
AmsaleTsige <amsale324@gmail.com>
2022-07-22 07:03:16 -0700

    Argc argv

---
+++ 0x09-static_libraries/driver.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include "main.h"
+int main(void)
+{
+int addtion = add (4, 8);
+printf("sum = %d\n", addtion);
+return 0;
+}
---
+++ 0x09-static_libraries/lib_newfile.c
@@ -0,0 +1,9 @@
+#include <stdio.h>                                                  
+
+int add(int a, int b)                                               
+
+{                                                                  
+
+return a+b;                                                         
+
+}
