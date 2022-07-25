37d5b8a01b9ac7c76d95ff8ec40fa74a7a2eb19e
AmsaleTsige <amsale324@gmail.com>
2022-07-25 00:59:56 -0700

    2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -15,8 +15,10 @@ if (s1 == NULL)
 s1 = "";
 if (s2 == NULL)
 s2 = "";
-for (i = 0; s1[i] != '\0'; i++);
-for (j = 0; s2[j] != '\0'; j++);
+for (i = 0; s1[i] != '\0'; i++)
+;
+for (j = 0; s2[j] != '\0'; j++)
+;
 strout = malloc(sizeof(char) * (i + j + 1));
 if (strout == NULL)
 {
