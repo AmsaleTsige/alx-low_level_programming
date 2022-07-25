c2e71ad7de16987c21496571c41703f68c10e01e
AmsaleTsige <amsale324@gmail.com>
2022-07-25 01:02:06 -0700

    1-strdup.c

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -11,7 +11,8 @@ char *strout;
 unsigned int i, j;
 if (str == NULL)
 return (NULL);
-for (i = 0; str[i] != '\0'; i++);
+for (i = 0; str[i] != '\0'; i++)
+;
 strout = (char *)malloc(sizeof(char) * (i + 1));
 if (strout == NULL)
 return (NULL);
