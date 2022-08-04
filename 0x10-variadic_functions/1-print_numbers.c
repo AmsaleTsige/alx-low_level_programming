d227e1c45de5ed62e689bbfecb3498f2b04d30ed
AmsaleTsige <amsale324@gmail.com>
2022-08-04 05:31:17 -0700

    3-print_all.c

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -1,16 +1,16 @@
-/*
+/**
  * File: 1-print_numbers.c
  * Auth: Tolulope Fakunle
  */
 #include "variadic_functions.h"
 #include <stdio.h>
 #include <stdarg.h>
-/*
+/**
  * print_numbers - Prints numbers, followed by a new line.
  * @separator: The string to be printed between numbers.
  * @n: The number of integers passed to the function.
  * @...: A variable number of numbers to be printed.
- */
+*/
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
 va_list nums;
--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -7,18 +7,18 @@
 #include <stdio.h>
 #include <stdarg.h>
 
- void print_char(va_list arg);
- void print_int(va_list arg);
- void print_float(va_list arg);
- void print_string(va_list arg);
- void print_all(const char * const format, ...);
+void print_char(va_list arg);
+void print_int(va_list arg);
+void print_float(va_list arg);
+void print_string(va_list arg);
+void print_all(const char * const format, ...);
 
- /**
-  * print_char - Prints a char.
-  * @arg: A list of arguments pointing to
-  * the character to be printed.
-  */
- void print_char(va_list arg)
+/**
+ * print_char - Prints a char.
+ * @arg: A list of arguments pointing to
+ * the character to be printed.
+ */
+void print_char(va_list arg)
 {
 char letter;
 letter = va_arg(arg, int);
