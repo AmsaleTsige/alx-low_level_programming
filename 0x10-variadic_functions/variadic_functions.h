57b32d6f45ad595b7f983df00a29c72c0ae2ce44
AmsaleTsige <amsale324@gmail.com>
2022-08-04 06:35:05 -0700

    variadic_functions.h

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,31 +1,26 @@
-#ifndef VARIADIC_FUNCTIONS_H
-#define VARIADIC_FUNCTIONS_H
-
-/*
- * File: variadic_functions.h
- * Auth: Tolulope Fakunle
- * Desc: Header file containing prototypes for all functions
- * used in the 0x0F-variadic_functions directory.
- */
-
+#ifndef _VARIADIC_FUNCTIONS_H
+#define _VARIADIC_FUNCTIONS_H
 #include <stdarg.h>
-
+#include <stdio.h>
 /**
- * struct printer - A new struct type defining a printer.
- * @symbol: A symbol representing a data type.
- * @print: A function pointer to a function that prints
- * a data type corresponding to symbol.
+ * struct fmt - Struct format
+ * @f: type format
+ * @p: print function
  */
-typedef struct printer
+typedef struct fmt
 {
-char *symbol;
-void (*print)(va_list arg);
-
-} printer_t;
-
+char *f;
+void (*p)(va_list s);
+} my_fmt;
+/*prototypes*/
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
+int _strlen_recursion(char *s);
+void print_c(va_list s);
+void print_i(va_list s);
+void print_f(va_list s);
+void print_s(va_list s);
 
 #endif
