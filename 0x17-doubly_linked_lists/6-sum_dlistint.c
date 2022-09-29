3fb1a62578af72546d754cabcc9dd4d06dfd78dd
root <you@example.com>
2022-09-29 13:02:21 -0700

    Sum list

--- 0x17-doubly_linked_lists/6-sum_dlistint.c
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -7,12 +7,12 @@
  **/
 int sum_dlistint(dlistint_t *head)
 {
-	dlistint_t *node = head;
-	int sum = 0;
-	while (node)
-	{
-		sum += node->n;
-		node = node->next;
-	}
-	return (sum);
+dlistint_t *node = head;
+int sum = 0;
+while (node)
+{
+sum += node->n;
+node = node->next;
+}
+return (sum);
 }
