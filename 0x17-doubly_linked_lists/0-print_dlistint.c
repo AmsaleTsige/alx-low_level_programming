31d4b3d58fa3a1519bb7ded66b5dba90f8da6566
root <you@example.com>
2022-09-29 12:51:58 -0700

    Task 0

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -6,12 +6,12 @@
  */
 size_t print_dlistint(const dlistint_t *h)
 {
-	const dlistint_t *temp;
-	size_t count;
-	temp = h;
-	while (temp && temp->prev)
-		temp = temp->prev;
-	for (count = 0; temp; count++, temp = temp->next)
-		printf("%d\n", temp->n);
-	return (count);
+const dlistint_t *temp;
+size_t count;
+temp = h;
+while (temp && temp->prev)
+temp = temp->prev;
+for (count = 0; temp; count++, temp = temp->next)
+printf("%d\n", temp->n);
+return (count);
 }
