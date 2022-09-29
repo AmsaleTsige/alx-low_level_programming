d6bb0ff939be72778ac765ba08d9fd486ed72482
root <you@example.com>
2022-09-29 12:53:44 -0700

    List length

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -6,15 +6,12 @@
  **/
 size_t dlistint_len(const dlistint_t *h)
 {
-	const dlistint_t *node = h;
-	size_t cont = 0;
-	
-	while (node)
-	{
-
-		cont++;
-		node = node->next;
-	}
-
-	return (cont);
+const dlistint_t *node = h;
+size_t cont = 0;
+while (node)
+{
+cont++;
+node = node->next;
+}
+return (cont);
 }
