331e3734425b3f1df0e61a74adb401b11e006352
root <you@example.com>
2022-09-30 11:44:22 -0700

    7-insert_dnodeint.c

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -10,40 +10,40 @@
  **/
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	dlistint_t *aux_node = *h, *new_node;
-	unsigned int index, cont = 0;
-	/* create node */
-	new_node = malloc(sizeof(dlistint_t));
-	if (new_node == NULL)
-		return (NULL);
-	new_node->n = n;
-	/* border case for insert at the beginning */
-	if (idx == 0)
-	{
-		new_node->prev = NULL;
-		new_node->next = *h;
-		if (*h)
-			(*h)->prev = new_node;
-		*h = new_node;
-		return (*h);
-	}
-	/* search of position to insert */
-	index = idx - 1;
-	while (aux_node && cont != index)
-	{
-		cont++;
-		aux_node = aux_node->next;
-	}
-	/* general case */
-	if (cont == index && aux_node)
-	{
-		new_node->prev = aux_node;
-		new_node->next = aux_node->next;
-		if (aux_node->next)
-			aux_node->next->prev = new_node;
-		aux_node->next = new_node;
-		return (new_node);
-	}
-	free(new_node);
-	return (NULL);
+dlistint_t *aux_node = *h, *new_node;
+unsigned int index, cont = 0;
+/* create node */
+new_node = malloc(sizeof(dlistint_t));
+if (new_node == NULL)
+return (NULL);
+new_node->n = n;
+/* border case for insert at the beginning */
+if (idx == 0)
+{
+new_node->prev = NULL;
+new_node->next = *h;
+if (*h)
+(*h)->prev = new_node;
+*h = new_node;
+return (*h);
+}
+/* search of position to insert */
+index = idx - 1;
+while (aux_node && cont != index)
+{
+cont++;
+aux_node = aux_node->next;
+}
+/* general case */
+if (cont == index && aux_node)
+{
+new_node->prev = aux_node;
+new_node->next = aux_node->next;
+if (aux_node->next)
+aux_node->next->prev = new_node;
+aux_node->next = new_node;
+return (new_node);
+}
+free(new_node);
+return (NULL);
 }
