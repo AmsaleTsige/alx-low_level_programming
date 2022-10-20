d54912fe829a8440cbe14eb63e6b2d9dd465bbcd
root <you@example.com>
2022-10-20 12:18:21 -0700

    4-hash_table_get.c

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -8,17 +8,17 @@
  */
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
-	unsigned int pos;
-	hash_node_t *actual;
-	if (!ht || !key || !(*key))
-		return (NULL);
-	pos = key_index((unsigned char *)key, ht->size);
-	actual = ht->array[pos];
-	while (actual)
-	{
-		if (!strcmp(key, actual->key))
-			return (actual->value);
-		actual = actual->next;
-	}
-	return (NULL);
+unsigned int pos;
+hash_node_t *actual;
+if (!ht || !key || !(*key))
+return (NULL);
+pos = key_index((unsigned char *)key, ht->size);
+actual = ht->array[pos];
+while (actual)
+{
+if (!strcmp(key, actual->key))
+return (actual->value);
+actual = actual->next;
+}
+return (NULL);
 }
